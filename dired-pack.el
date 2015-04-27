505d9fd63bda7ab88d5f97728830ff78e14d9de6
Pan, Senshan <pansenshan@gmail.com>
2015-04-27 23:08:26 +0800

    dired-tar -> dired-pack

--- dired-pack.el
+++ dired-pack.el
@@ -38,8 +38,8 @@
 ;;;      whatever).  The default is 'vf'; I'm told Windows users
 ;;;      should use "mvf".
 ;;;
-;;; dired-tar-gzip-extension - extension to use for gzipped tar files.
-;;;      Defaults to ".tgz", but ".tar.gz" may be a standard value in
+;;; dired-pack-extension - extension to use for gzipped tar files.
+;;;      Defaults to ".tgz", but ".7z" may be a useful value in
 ;;;      some circumstances.
 ;;;
 ;;; dired-tar-gzip-command - a shell command which gzips its
@@ -77,7 +77,7 @@ nil if it should leave them alone.")
 
 (defvar dired-pack-extension ".tgz"
   "*File name extension to use for gzipped tar files.
-By default, this is \".tgz\", but some people may like to use \".tar.gz\".")
+By default, this is \".tgz\", but some people may like to use \".7z\".")
 
 (defvar dired-tar-gzip-command "gzip --best --stdout"
   "*A shell command which gzips its standard input to its standard output.")
@@ -100,8 +100,8 @@ This is concatenated with command characters (\"x\" or \"c\" or
 whatever).  The default is 'vf'; I'm told Windows users should use
 \"mvf\".")
 
-(defvar dired-tar-result nil
-  "For internal use by dired-tar functions.
+(defvar dired-pack-result nil
+  "For internal use by dired-pack functions.
 This variable is made local to the buffer in which we run the tar
 process, and holds the name of the file created or affected.  The
 process-termination sentinal uses this to update the dired listing
@@ -113,12 +113,12 @@ when the process completes its work, or dies.")
 ;;;; Internal functions.
 
 (defun dired-pack-run-command (command directory result)
-  "Internal function for use by the dired-tar package.
+  "Internal function for use by the dired-pack package.
 Run COMMAND asynchronously in its own window, like a compilation.
 Use DIRECTORY as the default directory for the command's execution.
 RESULT is the name of the tar file which will be created, or the
 name of the directory into which the tar file was unpacked."
-  (let ((buf (dired-tar-get-buffer)))
+  (let ((buf (dired-pack-get-buffer)))
     (save-excursion
       (set-buffer buf)
       (setq buffer-read-only nil)
@@ -134,12 +134,12 @@ name of the directory into which the tar file was unpacked."
             mode-name "Tar-Output"
             default-directory directory)
 
-      (set (make-local-variable 'dired-tar-result)
+      (set (make-local-variable 'dired-pack-result)
            result)
       (set (make-local-variable 'mode-line-process)
            '(": %s"))
       (set (make-local-variable 'compilation-finish-function)
-           'dired-tar-operation-done)
+           'dired-pack-operation-done)
 
       (let ((process
              ;; Chris Moore <Chris.Moore@src.bae.co.uk> says that the
@@ -151,7 +151,7 @@ name of the directory into which the tar file was unpacked."
         (set-process-sentinel process 'compilation-sentinel))
       (display-buffer buf))))
 
-(defun dired-tar-get-buffer ()
+(defun dired-pack-get-buffer ()
   "Choose a buffer to run a tar process in.
 Tar output buffers have names like *Tar*, *Tar*<2>, *Tar*<3>, ...
 We return the lowest-numbered buffer that doesn't have a live tar
@@ -176,34 +176,34 @@ deleted."
   (generate-new-buffer "*Tar*"))
 
 
-(defun dired-tar-operation-done (buf message)
-  "Internal function for use by the dired-tar package.
+(defun dired-pack-operation-done (buf message)
+  "Internal function for use by the dired-pack package.
 This function is run when the tar operation completes.  It tries to
-update the dired listing by looking at dired-tar-result."
+update the dired listing by looking at dired-pack-result."
   (cond
-   ((null dired-tar-result))
+   ((null dired-pack-result))
 
-   ((file-directory-p dired-tar-result)
+   ((file-directory-p dired-pack-result)
     (save-excursion
       (mapcar
        (function (lambda (buf)
                    (set-buffer buf)
                    (dired-revert)))
-       (dired-buffers-for-dir dired-tar-result))))
+       (dired-buffers-for-dir dired-pack-result))))
 
-   ((file-exists-p dired-tar-result)
-    (dired-relist-file dired-tar-result))
+   ((file-exists-p dired-pack-result)
+    (dired-relist-file dired-pack-result))
 
    ;; Otherwise, I guess the tar operation must have failed somehow.
    ))
 
 (defun dired-pack (directory prefix-arg)
-  "Internal function for use by the dired-tar package.
+  "Internal function for use by the dired-pack package.
 Create a gzipped tar file from the contents of DIRECTORY.
 The archive is named after the directory, and the files are stored in
 the archive with names relative to DIRECTORY's parent.
 
-We use `dired-tar-gzip-extension' as the suffix for the filenames we
+We use `dired-pack-extension' as the suffix for the filenames we
 create.
 
 For example, (dired-pack \"/home/blandy/womble/\") would produce a
@@ -222,7 +222,7 @@ The second argument PREFIX-ARG is ignored."
      parent-name
      tar-file-name)))
 
-(defconst dired-tar-tarfile-regexp
+(defconst dired-pack-regexp
   (format "\\(%s\\)\\'"
           (mapconcat 'regexp-quote
                      '(".tar$" ".tar.z$" ".tar.gz$" ".tar.Z$" ".tgz$" ".rar$" ".zip$" ".7z$")
@@ -237,7 +237,7 @@ The second argument PREFIX-ARG is ignored."
   "Regular expression matching plausible filenames for compressed files.")
 
 (defun dired-unpack (tar-file prefix-arg)
-  "Internal function for use by the dired-tar package.
+  "Internal function for use by the dired-pack package.
 Unpack TAR-FILE into the directory containing it.
 If PREFIX-ARG is non-nil, just list the archive's contents without
 unpacking it."
@@ -247,7 +247,7 @@ unpacking it."
     (dired-pack-run-command
      (cond
       ;; Does this look like a tar file at all?
-      ((not (string-match dired-tar-tarfile-regexp tar-file))
+      ((not (string-match dired-pack-regexp tar-file))
        (error
         "bug: dired-unpack should only be passed tar file names."))
 
@@ -308,7 +308,7 @@ file out of its contents.
 
   (let ((filename (dired-get-filename)))
     (cond
-     ((string-match dired-tar-tarfile-regexp filename)
+     ((string-match dired-pack-regexp filename)
       (dired-unpack filename prefix-arg))
 
      (t
@@ -328,4 +328,4 @@ file out of its contents.
 
 (provide 'dired-pack)
 
-;;; dired-tar.el ends here
+;;; dired-pack.el ends here
